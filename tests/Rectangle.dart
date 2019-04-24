4b683a670f3095516627b4dbf35e1b4a1251ca43
Dario Gabriel <dariogabriel113@gmail.com>
2019-04-24 15:03:01 -0300

    Add constructor to the Rectangle class

--- tests/Rectangle.dart
+++ tests/Rectangle.dart
@@ -4,6 +4,8 @@ class Rectangle {
   int width;
   int height;
   Point origin;
+
+  Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
 }
 
 main() {} // Included main() to suppress uncaught exception.
