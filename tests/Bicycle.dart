8c344e57bcb683558f23763f70c865601c8954a9
Dario Gabriel <dariogabriel113@gmail.com>
2019-04-24 14:38:27 -0300

    Add getter to the Bicycle class

--- tests/Bicycle.dart
+++ tests/Bicycle.dart
@@ -1,15 +1,17 @@
 class Bicycle {
   int cadence;
-  int speed;
+  int _speed = 0;
   int gear;
-  
-  Bicycle(this.cadence, this.speed, this.gear);
-  
+
+  Bicycle(this.cadence, this.gear);
+
   @override
   String toString() => 'Bicycle: $speed mph';
+
+  int get speed => _speed;
 }
 
 void main() {
-  var bike = new Bicycle(2, 0, 1);
+  var bike = new Bicycle(2, 1);
   print(bike);
 }
