e7209fc6159fb1ce341aa840cf3a9098573dce4a
Dario Gabriel <dariogabriel113@gmail.com>
2019-04-24 14:43:09 -0300

    Add applyBrake and speedUp to the Bicycle class

--- tests/Bicycle.dart
+++ tests/Bicycle.dart
@@ -5,10 +5,18 @@ class Bicycle {
 
   Bicycle(this.cadence, this.gear);
 
-  @override
-  String toString() => 'Bicycle: $speed mph';
-
   int get speed => _speed;
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
 }
 
 void main() {
